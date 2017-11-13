b6e0009e71fde74fa2048adeac1924b322d5a44a
luis jose sanchez carrasco <luissancar@yahoo.es>
2017-11-13 12:38:10 +0100

    Create LeerC.kt

---
+++ app/src/main/java/com/example/luisjosesanchezcarrasco/firebaseandroidkotlin/LeerC.kt
@@ -0,0 +1,12 @@
+class LeerC() {
+    public fun convierte(): Pair<String,String> {
+        var cadenaentera="{'uno','dos'}"
+        var cadena=(cadenaentera.substringAfter("{")).substringBefore("}")
+        var uno=(cadena.substringAfter("'")).substringBefore("'")
+
+
+         cadena=(cadenaentera.substringAfter(",")).substringBefore("}")
+        var dos=(cadena.substringAfter("'")).substringBefore("'")
+        return Pair(uno, dos)
+    }
+}
